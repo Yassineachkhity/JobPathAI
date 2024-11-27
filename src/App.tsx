4858ac9d3b67a836b1b84862dddeea10a4dbd5fa
Yassine Achkhity <yassineachkhity56@gmail.com>
2024-11-27 03:08:09 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/tailwind.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white text-center p-10">
+          <h1 className="text-4xl font-bold">JobPath AI</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
